0aaedeede0f908e4011e20156a82b714f659f423
weijiangyang <weijiangyang@laposte.net>
2021-05-25 14:55:43 +0200

    --- Auto-Git Commit ---

--- MVP/app1.js
+++ MVP/app1.js
@@ -15,25 +15,25 @@ function getPelucheInf(){
         document.querySelector("div#peluche #forth").src = value[3].imageUrl;
         document.querySelector("div#peluche #fifth").src = value[4].imageUrl;
 
-        document.querySelector("div.first-peluche h2#firstitle").innerHTML = value[0].name;
+        document.querySelector("div.first-peluche h3#firstitle").innerHTML = value[0].name;
         document.querySelector("div.first-peluche p#firstdesp").innerHTML = value[0].description;
-        document.querySelector("div.first-peluche p#firstprice").innerHTML = `PRICE:${value[0].price}€`;
+        document.querySelector("div.first-peluche p#firstprice").innerHTML = `Price:${(value[0].price/100).toFixed(2)}€`;
 
-        document.querySelector("div.second-peluche h2#secondtitle").innerHTML = value[1].name;
+        document.querySelector("div.second-peluche h3#secondtitle").innerHTML = value[1].name;
         document.querySelector("div.second-peluche p#seconddesp").innerHTML = value[1].description;
-        document.querySelector("div.second-peluche p#secondprice").innerHTML = `PRICE:${value[1].price}€`;
+        document.querySelector("div.second-peluche p#secondprice").innerHTML = `Price:${(value[1].price/100).toFixed(2)}€`;
 
-        document.querySelector("div.third-peluche h2#thirdtitle").innerHTML = value[2].name;
+        document.querySelector("div.third-peluche h3#thirdtitle").innerHTML = value[2].name;
         document.querySelector("div.third-peluche p#thirddesp").innerHTML = value[2].description;
-        document.querySelector("div.third-peluche p#thirdprice").innerHTML = `PRICE:${value[2].price}€`;
+        document.querySelector("div.third-peluche p#thirdprice").innerHTML = `Price:${(value[2].price/100).toFixed(2)}€`;
 
-        document.querySelector("div.forth-peluche h2#forthtitle").innerHTML = value[3].name;
+        document.querySelector("div.forth-peluche h3#forthtitle").innerHTML = value[3].name;
         document.querySelector("div.forth-peluche p#forthdesp").innerHTML = value[3].description;
-        document.querySelector("div.forth-peluche p#forthprice").innerHTML = `PRICE:${value[3].price}€`;
+        document.querySelector("div.forth-peluche p#forthprice").innerHTML = `Price:${(value[3].price/100).toFixed(2)}€`;
 
-        document.querySelector("div.forth-peluche h2#forthtitle").innerHTML = value[3].name;
-        document.querySelector("div.forth-peluche p#forthdesp").innerHTML = value[3].description;
-        document.querySelector("div.forth-peluche p#forthprice").innerHTML = `PRICE:${value[3].price}€`;
+        document.querySelector("div.fifth-peluche h3#fifthtitle").innerHTML = value[4].name;
+        document.querySelector("div.fifth-peluche p#fifthdesp").innerHTML = value[4].description;
+        document.querySelector("div.fifth-peluche p#fifthprice").innerHTML = `Price:${(value[4].price/100).toFixed(2)}€`;
 
         
 
