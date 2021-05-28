c75f1c67a46524e5208a102e12d266f70492a5e1
weijiangyang <weijiangyang@laposte.net>
2021-05-28 10:28:12 +0200

    --- Auto-Git Commit ---

--- MVP/app3.js
+++ MVP/app3.js
@@ -7,10 +7,25 @@ for ( let produit of produits){
 console.log(produit.color)
 // const elt= document.createElement("div");
 
+let eltP = document.createElement("div");
+let parentD = document.getElementById("container");
+parentD.appendChild(eltP);
+let eltPimg = document.createElement("img");
+eltP.appendChild(eltPimg);
+eltPimg.src = produit.src;
+eltPimg.setAttribute("width","20%");
+let eltPname = document.createElement("h3");
+eltP.appendChild(eltPname);
+eltPname.innerText = produit.name;
+ let eltPdesp = document.createElement("p");
+ eltPdesp.setAttribute("id","description")
+ eltP.appendChild(eltPdesp);
+ eltPdesp.innerHTML = produit.description ;
+
+
 }
-let eltP = document.querySelector("h2");
-// parentDiv.appendChild(elt);
-eltP.InnerHTML = "q45";
+
+
 
 
 
