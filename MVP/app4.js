5fda8f1693dc739a65bf65bfa2e91ff7483d0865
weijiangyang <weijiangyang@laposte.net>
2021-05-29 21:48:08 +0200

    --- Auto-Git Commit ---

--- MVP/app4.js
+++ MVP/app4.js
@@ -7,34 +7,40 @@ function getPelucheInf(){
     })
     .then(function(value){
         let ids=[];
-        console.log(value.length);
+        // console.log(value.length);
         for (let i=0; i<value.length;i++){
             let id=value[i]._id;
             ids.push(id);
         };
         // console.log(ids)
         for (let id of ids){
+            let listProduits = [];
             fetch("http://localhost:3000/api/teddies"+"/"+id)
             .then(function(res){
                 if(res.ok){
                     return res.json();}})
             .then(function(value){
-                console.log(value);
-                let listProduits = [];
-                for(let i=0; i<value.length;i++){
-                    listProduits.push(value[i]);
-
-                }
                 
+                
+                listProduits.push(value);
+                
+               
             })
             .catch(function(err){
                 console.log("il y a un error")
-            })}
+            })
+        console.log(listProduits);
         
-        
+     };
+     
+     
+    })       
 
+        
+        
+            
     
-    })
+    
     .catch(function(err){
         console.log("il y a un error")
     })
