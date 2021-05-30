08a41e499f903e491b107524fd046e9e00ef7ddc
weijiangyang <weijiangyang@laposte.net>
2021-05-30 21:55:56 +0200

    --- Auto-Git Commit ---

--- MVP/app4.js
+++ MVP/app4.js
@@ -55,12 +55,27 @@ let listPeluches= JSON.parse(localStorage.getItem("listPeluches"));
 let parentP = document.querySelector("main");
 for (let i=0; i<listPeluches.length;i++){
     let eltP = document.createElement("div");
+    eltP.setAttribute("id",`peluche-${i}`)
     parentP.appendChild(eltP);
-    let image = document.createElement("img");
-    parentP.appendChild(image)
-    document.querySelector("img").src=listPeluches[0].imageUrl;
-    
-}
+    let eltImage = document.createElement("img");
+    eltImage.setAttribute("id",`image-${i}`)
+    eltP.appendChild(eltImage)
+    document.getElementById(`image-${i}`).src=listPeluches[i].imageUrl;
+    let eltH3 = document.createElement("h3");
+    eltH3.setAttribute("id", `title-${i}`);
+    eltP.appendChild(eltH3);
+    document.getElementById(`title-${i}`).innerHTML=listPeluches[i].name;
+    let eltD = document.createElement("p");
+    eltD.setAttribute("id",`description-${i}`);
+    eltP.appendChild(eltD);
+    document.getElementById(`description-${i}`).innerHTML = listPeluches[i].description;
+    let eltPrice = document.createElement("p");
+    eltPrice.setAttribute("id",`price-${i}`);
+    eltP.appendChild(eltPrice);
+    document.getElementById(`price-${i}`).innerHTML =`Price:${(listPeluches[i].price/100).toFixed(2)}€`
+
+ }   
+
 
 
 
