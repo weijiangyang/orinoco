a54ff7626fc55f7517668ba0da5aaa87a1eb0fdc
weijiangyang <weijiangyang@laposte.net>
2021-05-30 22:56:55 +0200

    --- Auto-Git Commit ---

--- MVP/app4.js
+++ MVP/app4.js
@@ -53,33 +53,38 @@ function getPelucheInf(){
 getPelucheInf();
 let listPeluches= JSON.parse(localStorage.getItem("listPeluches"));
 let parentP = document.querySelector("main");
-parentP.style.backgroundColor = 'blue';
+
 parentP.style.display = "flex";
 parentP.style.flexWrap = "wrap";
 parentP.style.justifyItems = 'center';
 
 for (let i=0; i<listPeluches.length;i++){
     let eltP = document.createElement("div");
-    eltP.setAttribute("id",`peluche-${i}`)
+    eltP.setAttribute("id",`peluche-${i}`);
+    eltP.setAttribute("class","peluche-carte")
     parentP.appendChild(eltP);
     eltP.style.height = "450px";
     eltP.style.width = "30%";
-    eltP.style.backgroundColor ="red";
+   
     let eltImage = document.createElement("img");
-    eltImage.setAttribute("id",`image-${i}`)
+    eltImage.setAttribute("id",`image-${i}`);
+    eltImage.setAttribute("class","peluche-image")
     eltP.appendChild(eltImage);
     eltImage.style.height = "320px"
     document.getElementById(`image-${i}`).src=listPeluches[i].imageUrl;
     let eltH3 = document.createElement("h3");
     eltH3.setAttribute("id", `title-${i}`);
+    eltH3.setAttribute("class","peluche-titre")
     eltP.appendChild(eltH3);
     document.getElementById(`title-${i}`).innerHTML=listPeluches[i].name;
     let eltD = document.createElement("p");
     eltD.setAttribute("id",`description-${i}`);
+    eltD.setAttribute("class","peluche-desp");
     eltP.appendChild(eltD);
     document.getElementById(`description-${i}`).innerHTML = listPeluches[i].description;
     let eltPrice = document.createElement("p");
     eltPrice.setAttribute("id",`price-${i}`);
+    eltPrice.setAttribute("class","peluche-price")
     eltP.appendChild(eltPrice);
     document.getElementById(`price-${i}`).innerHTML =`Price:${(listPeluches[i].price/100).toFixed(2)}€`
 
