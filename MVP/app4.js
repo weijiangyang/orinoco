6d9c934873714e3d4a571917bc2372cae0ef6cf8
weijiangyang <weijiangyang@laposte.net>
2021-05-31 16:22:10 +0200

    --- Auto-Git Commit ---

--- MVP/app4.js
+++ MVP/app4.js
@@ -25,6 +25,51 @@ function getPelucheInf(){
             .then(function(value){
                 
                 localStorage.setItem(`peluche-${id}`,JSON.stringify(value));
+
+for (let i=0; i<localStorage.length;i++){
+
+    let eltA = document.createElement("a");
+    eltA.setAttribute("id",`lien-${i}`);
+    eltA.setAttribute("class","lien");
+    parentP.appendChild(eltA);
+    document.getElementById(`lien-${i}`).href="page2.html";
+    eltA.addEventListener("click",function(){
+       let p = JSON.parse(localStorage.getItem(`peluche-${id}`));
+        localStorage.setItem("optionItem",JSON.stringify(p))
+        
+        
+    });
+   
+    let eltP = document.createElement("div");
+    eltP.setAttribute("id",`peluche-${id}`);
+    eltP.setAttribute("class","peluche-carte")
+    eltA.appendChild(eltP);
+    eltP.style.height = "450px";
+    
+   
+    let eltImage = document.createElement("img");
+    eltImage.setAttribute("id",`image-${id}`);
+    eltImage.setAttribute("class","peluche-image")
+    eltP.appendChild(eltImage);
+    eltImage.style.height = "320px"
+    document.getElementById(`image-${id}`).src=p.imageUrl;
+    let eltH3 = document.createElement("h3");
+    eltH3.setAttribute("id", `title-${id}`);
+    eltH3.setAttribute("class","peluche-titre")
+    eltP.appendChild(eltH3);
+    document.getElementById(`title-${id}`).innerHTML=p.name;
+    let eltD = document.createElement("p");
+    eltD.setAttribute("id",`description-${id}`);
+    eltD.setAttribute("class","peluche-desp");
+    eltP.appendChild(eltD);
+    document.getElementById(`description-${id}`).innerHTML = p.description;
+    let eltPrice = document.createElement("p");
+    eltPrice.setAttribute("id",`price-${id}`);
+    eltPrice.setAttribute("class","peluche-price")
+    eltP.appendChild(eltPrice);
+    document.getElementById(`price-${id}`).innerHTML =`Price:${(p.price/100).toFixed(2)}€`
+
+ }   
                
                 })
             
@@ -34,6 +79,7 @@ function getPelucheInf(){
         
         
      };
+
      
     
     })       
@@ -55,50 +101,7 @@ parentP.style.display = "flex";
 parentP.style.flexWrap = "wrap";
 parentP.style.justifyItems = 'center';
 
-for (let i=0; i<localStorage.length;i++){
 
-    let eltA = document.createElement("a");
-    eltA.setAttribute("id",`lien-${i}`);
-    eltA.setAttribute("class","lien");
-    parentP.appendChild(eltA);
-    document.getElementById(`lien-${i}`).href="page2.html";
-    // eltA.addEventListener("click",function(){
-    //    let p = JSON.parse(localStorage.getItem("listPeluches"));
-    //     localStorage.setItem("optionProduit",JSON.stringify(p[i]))
-        
-        
-    // });
-   
-    let eltP = document.createElement("div");
-    eltP.setAttribute("id",`peluche-${i}`);
-    eltP.setAttribute("class","peluche-carte")
-    eltA.appendChild(eltP);
-    eltP.style.height = "450px";
-    
-   
-    let eltImage = document.createElement("img");
-    eltImage.setAttribute("id",`image-${i}`);
-    eltImage.setAttribute("class","peluche-image")
-    eltP.appendChild(eltImage);
-    eltImage.style.height = "320px"
-    document.getElementById(`image-${i}`).src=listPeluches[i].imageUrl;
-    let eltH3 = document.createElement("h3");
-    eltH3.setAttribute("id", `title-${i}`);
-    eltH3.setAttribute("class","peluche-titre")
-    eltP.appendChild(eltH3);
-    document.getElementById(`title-${i}`).innerHTML=listPeluches[i].name;
-    let eltD = document.createElement("p");
-    eltD.setAttribute("id",`description-${i}`);
-    eltD.setAttribute("class","peluche-desp");
-    eltP.appendChild(eltD);
-    document.getElementById(`description-${i}`).innerHTML = listPeluches[i].description;
-    let eltPrice = document.createElement("p");
-    eltPrice.setAttribute("id",`price-${i}`);
-    eltPrice.setAttribute("class","peluche-price")
-    eltP.appendChild(eltPrice);
-    document.getElementById(`price-${i}`).innerHTML =`Price:${(listPeluches[i].price/100).toFixed(2)}€`
-
- }   
 
  
 
