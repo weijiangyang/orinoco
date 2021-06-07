cde6a8b39fbb34def6b82796eb5d5f81f56202fb
weijiangyang <weijiangyang@laposte.net>
2021-06-07 12:00:37 +0200

    --- Auto-Git Commit ---

--- MVP/commande.js
+++ MVP/commande.js
@@ -1,9 +1,19 @@
-let orderId = document.getElementById("idoforder");
+
 let monCommande = JSON.parse(localStorage.getItem("monCommande"));
+if(monCommande ==null || monCommande.length == 0){
+    document.querySelector("h2").innerText = "Vous n'avez aucune commande en cours!";
+    document.querySelector("p.infocommande").innerText = "Ajouter d'abord vos produits au votre panier et puis valider vos commandes"
+}else{
+let orderId = document.getElementById("idoforder");
 orderId.innerText = monCommande.orderId;
 let priceTotalCommande = document.getElementById("priceoforder");
+let listOfProductsCommanded = monCommande.listOfProductsCommanded;
+
+
 let sum = 0;
-for(let i=0; i<monCommande.length;i++){
+for(let i=0; i<listOfProductsCommanded.length;i++){
+    let priceItem = (listOfProductsCommanded[i].price)*(listOfProductsCommanded[i].quantity)/100;
+    sum+=priceItem
 
 }
-priceTotalCommande.innerText = mon
+priceTotalCommande.innerText = `${sum.toFixed(2)}€`;}
