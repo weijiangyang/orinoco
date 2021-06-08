dc038eac881b8c936dfeeee74c78a4c9db323693
weijiangyang <weijiangyang@laposte.net>
2021-06-08 15:14:42 +0200

    --- Auto-Git Commit ---

--- MVP/panier-formulaire.js
+++ MVP/panier-formulaire.js
@@ -317,6 +317,25 @@ let formPurchaseOrder = new Object;
 }
 )
 
+document.getElementById("menu-pilier").addEventListener(
+    "click",function(){
+        // document.getElementById("menubar").style.display="none";
+        document.getElementById("menu-extend").style.display="block";
+        // document.getElementById("second-bar").style.display="none";
+        document.getElementById("third-bar").style.transformOrigin="left";
+        document.getElementById("first-bar").style.transformOrigin="left";
+        document.getElementById("first-bar").style.transform="rotateZ(28deg)";
+        document.getElementById("third-bar").style.transform="rotateZ(-28deg)";
+        document.getElementById("third-bar").style.transition=".2s";
+        document.getElementById("first-bar").style.transition=".2s";
+        
+        document.getElementById("second-bar").style.display="none";
+        
+        document.getElementById("first-bar").style.transition="left";
+        }
+        
+    
+)
 
 
 
