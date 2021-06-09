410c6e7010f073af45b1eb9f94d916900dc423b7
weijiangyang <weijiangyang@laposte.net>
2021-06-09 23:12:50 +0200

    update48

--- MVP/commande.js
+++ MVP/commande.js
@@ -4,38 +4,33 @@ if(monCommande ==null || monCommande.length == 0){
     document.querySelector("h2").innerText = "Vous n'avez aucune commande en cours!";
     document.querySelector("p#infocommande").innerText = "Ajouter d'abord vos produits au votre panier et puis valider vos commandes"
 }else{
-let orderId = document.getElementById("idoforder");
-orderId.innerText = monCommande.orderId;
-let priceTotalCommande = document.getElementById("priceoforder");
-let listOfProductsCommanded = monCommande.listOfProductsCommanded;
-
-
-let sum = 0;
-for(let i=0; i<listOfProductsCommanded.length;i++){
-    let priceItem = (listOfProductsCommanded[i].price)*(listOfProductsCommanded[i].quantity)/100;
-    sum+=priceItem
-
-}
-priceTotalCommande.innerText = `${sum.toFixed(2)}€`;};
-
-document.getElementById("menu-pilier").addEventListener(
-    "click",function(){
-        // document.getElementById("menubar").style.display="none";
+    let orderId = document.getElementById("idoforder");
+    orderId.innerText = monCommande.orderId;
+    let priceTotalCommande = document.getElementById("priceoforder");
+    let listOfProductsCommanded = monCommande.listOfProductsCommanded;
+    let sum = 0;
+    for(let i=0; i<listOfProductsCommanded.length;i++){
+        let priceItem = (listOfProductsCommanded[i].price)*(listOfProductsCommanded[i].quantity)/100;
+        sum+=priceItem
+    }
+    priceTotalCommande.innerText = `${sum.toFixed(2)}€`;
+};
+function menuPillier(){
+    document.getElementById("menu-pilier").addEventListener( "click",function(){
         document.getElementById("menu-extend").style.display="block";
-        // document.getElementById("second-bar").style.display="none";
         document.getElementById("third-bar").style.transformOrigin="left";
         document.getElementById("first-bar").style.transformOrigin="left";
         document.getElementById("first-bar").style.transform="rotateZ(28deg)";
         document.getElementById("third-bar").style.transform="rotateZ(-28deg)";
         document.getElementById("third-bar").style.transition=".2s";
         document.getElementById("first-bar").style.transition=".2s";
-        
         document.getElementById("second-bar").style.display="none";
-        
         document.getElementById("first-bar").style.transition="left";
         }
+     )   
+}            
         
-    
-)
+menuPillier();   
 
-// localStorage.removeItem("monCommande");
+
+
