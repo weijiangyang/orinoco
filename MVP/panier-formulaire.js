94d886761c67f19bd581e25ea6a21a7a6ae9e4e7
weijiangyang <weijiangyang@laposte.net>
2021-06-09 11:25:06 +0200

    --- Auto-Git Commit ---

--- MVP/panier-formulaire.js
+++ MVP/panier-formulaire.js
@@ -42,131 +42,113 @@ if(monPanier==null|| monPanier.length==0){
     eltValider.setAttribute("id","valider");
     parentMain.appendChild(eltValider);
     eltValider.innerHTML = "Ensuit vous pourrez valider votre panier Merci!";
-   
-  
-   
 }else{
+    let sum = 0;
+    for (let i=0;i<monPanier.length;i++){
+        parentD = document.createElement("div");
+        parentD.setAttribute("class","carte");
+        parentD.setAttribute("id",`carte-${i}`)
+        parentPanierInf.appendChild(parentD);
+        eltImage = document.createElement("div");
+        eltImage.setAttribute("class","image-produit");
+        parentD.appendChild(eltImage);
+        eltImg=document.createElement("img");
+        eltImg.src=monPanier[i].imageUrl;
+        eltImage.appendChild(eltImg);
+        eltT = document.createElement("div");
+        eltT.setAttribute("class","info-produit");
+        eltT.setAttribute("id",`infor-produit-${i}`)
+        parentD.appendChild(eltT);
+        eltN = document.createElement("h2");
+        eltN.setAttribute("id","nom-peluche")
+        eltT.appendChild(eltN);
+        eltN.innerHTML= monPanier[i].name;
+        eltD = document.createElement("p");
+        eltD.setAttribute("class","desp-produit");
+        eltD.setAttribute("id",`desp-produit-${i}`);
+        eltT.appendChild(eltD);
+        eltD.innerHTML = monPanier[i].description;
+        eltP = document.createElement("p");
+        eltP.setAttribute ("class","price-produit");
+        eltP.setAttribute("id",`price-produit-${i}`);
+        eltT.appendChild(eltP);
+        eltP.innerHTML = "Price:"+(monPanier[i].price/100).toFixed(2)+"&nbsp€"+`(*${monPanier[i].quantity}）`;
+        eltC = document.createElement("p");
+        eltC.setAttribute("class","color-produit");
+        eltC.setAttribute("id",`color-produit-${i}`)
+        eltT.appendChild(eltC);
+        eltC.innerHTML = "Color:"+monPanier[i].color;
+        eltLabel = document.createElement("label");
+        eltLabel.setAttribute("id",`label-${i}`);
+        eltT.appendChild(eltLabel);
+        eltLabel.innerHTML="Quantity: "
+        eltI = document.createElement("input");
+        eltI.setAttribute("class","quantity-inp");
+        eltLabel.appendChild(eltI);
+        eltI.setAttribute("type","number");
+        eltI.setAttribute("value",monPanier[i].quantity);
+        pItemTotal = document.createElement("p");
+        pItemTotal.setAttribute("id",`pItemTotal-${i}`);
+        pItemTotal.setAttribute("class","totalprice-produit");
+        eltT.appendChild(pItemTotal);
+        pItemTotal.innerHTML = ((monPanier[i].quantity)*(monPanier[i].price/100)).toFixed(2)+"&nbsp€";
+        sum+=(monPanier[i].quantity)*(monPanier[i].price/100);
+        btn = document.createElement("button");
+        eltT.appendChild(btn);
+        btn.innerHTML = "Supprimer";
+        btn.setAttribute("id","btn-supprimer");
+        btn.addEventListener("click",function(){
+        monPanier.splice(i,i+1);
+        localStorage.setItem("monPanier",JSON.stringify(monPanier));
+        location.reload();
+        }
+    )
+        priceTotal = document.createElement("p");
+        priceTotal.setAttribute("id","pricetotal")
+        parentMonpanier = document.querySelector("div.monpanier");
+        parentMonpanier.appendChild(priceTotal);
+        priceTotal.innerHTML = "Total:&nbsp<strong>"+sum.toFixed(2)+"&nbsp€"+"</strong>";
 
-let sum = 0;
-for (let i=0;i<monPanier.length;i++){
-   
-
-parentD = document.createElement("div");
-parentD.setAttribute("class","carte");
-parentD.setAttribute("id",`carte-${i}`)
-parentPanierInf.appendChild(parentD);
-
-eltImage = document.createElement("div");
-eltImage.setAttribute("class","image-produit");
-parentD.appendChild(eltImage);
-eltImg=document.createElement("img");
-eltImg.src=monPanier[i].imageUrl;
-eltImage.appendChild(eltImg);
-
-eltT = document.createElement("div");
-eltT.setAttribute("class","info-produit");
-eltT.setAttribute("id",`infor-produit-${i}`)
-parentD.appendChild(eltT);
-
-eltN = document.createElement("h2");
-eltN.setAttribute("id","nom-peluche")
-eltT.appendChild(eltN);
-eltN.innerHTML= monPanier[i].name;
-
-eltD = document.createElement("p");
-eltD.setAttribute("class","desp-produit");
-eltD.setAttribute("id",`desp-produit-${i}`);
-
-eltT.appendChild(eltD);
-eltD.innerHTML = monPanier[i].description;
-
-eltP = document.createElement("p");
-eltP.setAttribute ("class","price-produit");
-eltP.setAttribute("id",`price-produit-${i}`);
-eltT.appendChild(eltP);
-
-eltP.innerHTML = "Price:"+(monPanier[i].price/100).toFixed(2)+"&nbsp€"+`(*${monPanier[i].quantity}）`;
-eltC = document.createElement("p");
-eltC.setAttribute("class","color-produit");
-eltC.setAttribute("id",`color-produit-${i}`)
-eltT.appendChild(eltC);
-eltC.innerHTML = "Color:"+monPanier[i].color;
-
-eltLabel = document.createElement("label");
-eltLabel.setAttribute("id",`label-${i}`);
-eltT.appendChild(eltLabel);
-eltLabel.innerHTML="Quantity: "
-eltI = document.createElement("input");
-eltI.setAttribute("class","quantity-inp");
-eltLabel.appendChild(eltI);
-eltI.setAttribute("type","number");
-
-eltI.setAttribute("value",monPanier[i].quantity);
-
-pItemTotal = document.createElement("p");
-pItemTotal.setAttribute("id",`pItemTotal-${i}`);
-pItemTotal.setAttribute("class","totalprice-produit");
-eltT.appendChild(pItemTotal);
-pItemTotal.innerHTML = ((monPanier[i].quantity)*(monPanier[i].price/100)).toFixed(2)+"&nbsp€";
-sum+=(monPanier[i].quantity)*(monPanier[i].price/100);
-
-btn = document.createElement("button");
-eltT.appendChild(btn);
-btn.innerHTML = "Supprimer";
-btn.setAttribute("id","btn-supprimer");
-btn.addEventListener("click",function(){
-    monPanier.splice(i,i+1);
-    localStorage.setItem("monPanier",JSON.stringify(monPanier));
-    location.reload();
-    console.log(monPanier)
-})
-}
-
-console.log(sum);
-
-priceTotal = document.createElement("p");
-priceTotal.setAttribute("id","pricetotal")
-parentMonpanier = document.querySelector("div.monpanier");
-parentMonpanier.appendChild(priceTotal);
-priceTotal.innerHTML = "Total:&nbsp<strong>"+sum.toFixed(2)+"&nbsp€"+"</strong>";
-
-
-
-eltLienContinuer = document.createElement("a");
-eltLienContinuer.setAttribute("id","lien-continuer");
-eltLienContinuer.setAttribute("href","index.html");
-parentMonpanier.appendChild(eltLienContinuer);
-
-eltBtnContinue = document.createElement("div");
-eltBtnContinue.setAttribute("id","btn-continuer");
-eltLienContinuer.appendChild(eltBtnContinue);
-eltBtnContinue.innerHTML = "Continuer mon shopping";
-
-
-
-let p = document.getElementsByClassName("quantity-inp");
-for (let i=0;i<p.length; i++){
-
-
-   p[i].addEventListener("change",function(){
-        monPanier[i].quantity = p[i].value;
-       localStorage.setItem("monPanier",JSON.stringify(monPanier));
-       eltP = document.getElementById(`price-produit-${i}`);
-       eltP.innerHTML = "Price:"+(monPanier[i].price/100).toFixed(2)+"&nbsp€"+`(*${monPanier[i].quantity}）`;
-       pItemTotal = document.getElementById(`pItemTotal-${i}`);
-       pItemTotal.innerHTML = ((monPanier[i].quantity)*(monPanier[i].price/100)).toFixed(2)+"&nbsp€";
-       sum = 0;
-       for(let i=0;i<monPanier.length;i++){
-           sum+=(monPanier[i].quantity)*(monPanier[i].price/100);
-       }
-       console.log(sum);
-    //    eltTotal.innerHTML="Total:"+sum.toFixed(2)+"€";
-       priceTotal.innerHTML = "Total:&nbsp<strong>"+sum.toFixed(2)+"&nbsp€"+"</strong>";
-
-   })
+        eltLienContinuer = document.createElement("a");
+        eltLienContinuer.setAttribute("id","lien-continuer");
+        eltLienContinuer.setAttribute("href","index.html");
+        parentMonpanier.appendChild(eltLienContinuer);
     
-}
-}
+        eltBtnContinue = document.createElement("div");
+        eltBtnContinue.setAttribute("id","btn-continuer");
+        eltLienContinuer.appendChild(eltBtnContinue);
+        eltBtnContinue.innerHTML = "Continuer mon shopping";
+    
+        let p = document.getElementsByClassName("quantity-inp");
+        for (let i=0;i<p.length; i++){
+            p[i].addEventListener("change",function(){
+                monPanier[i].quantity = p[i].value;
+                localStorage.setItem("monPanier",JSON.stringify(monPanier));
+                eltP = document.getElementById(`price-produit-${i}`);
+                eltP.innerHTML = "Price:"+(monPanier[i].price/100).toFixed(2)+"&nbsp€"+`(*${monPanier[i].quantity}）`;
+                pItemTotal = document.getElementById(`pItemTotal-${i}`);
+                pItemTotal.innerHTML = ((monPanier[i].quantity)*(monPanier[i].price/100)).toFixed(2)+"&nbsp€";
+                sum = 0;
+                    for(let i=0;i<monPanier.length;i++){
+                        sum+=(monPanier[i].quantity)*(monPanier[i].price/100);
+                        }
+                    priceTotal.innerHTML = "Total:&nbsp<strong>"+sum.toFixed(2)+"&nbsp€"+"</strong>";
+                    }
+                )
+            }
+            }        
+    
+
+    
+
+   
+       
+       
+
+   
+    
+
+
 let form = document.getElementById("loginForm");
 form.email.addEventListener("change",function(){
     validEmail(this);
@@ -332,7 +314,7 @@ let formPurchaseOrder = new Object;
 }
 )
 
-
+}
     
 
 
