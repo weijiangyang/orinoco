ff82b53b88ed6504a5568d230c2e88c6153bf247
weijiangyang <weijiangyang@laposte.net>
2021-06-14 12:32:02 +0200

    --- Auto-Git Commit ---

--- MVP/listProduitNew.js
+++ MVP/listProduitNew.js
@@ -1,72 +1,71 @@
-function getPelucheInf(){
-    let listPeluches=[];
+function getpeulucheInf(){
+    let listpeuluches=[];
     fetch("http://localhost:3000/api/teddies")
     .then(function(res){
         if(res.ok){
             return res.json();
         }
     })
-    .then(function(peluches){
+    .then(function(peuluches){
         
         
-        for (let i=0; i<peluches.length;i++){
-            listPeluches.push(peluches[i])
+        for (let i=0; i<peuluches.length;i++){
+            listpeuluches.push(peuluches[i])
         };
-        localStorage.setItem("listPeluches",JSON.stringify(listPeluches));
+        localStorage.setItem("listpeuluches",JSON.stringify(listpeuluches));
     
 
 });
 }
 
 function elementAppend(){
-    getPelucheInf();
-    let listPeluches = JSON.parse(localStorage.getItem("listPeluches"));
-    for (let peuluche of listPeluches){
+    getpeulucheInf();
+    let listpeuluches = JSON.parse(localStorage.getItem("listpeuluches"));
+    for (let peuluche of listpeuluches){
         let carteLink = document.createElement("a");
         let totalProduits = document.querySelector("#total-produits");
-        carteLink.setAttribute("id",`link-${peluche._id}`);
+        carteLink.setAttribute("id",`link-${peuluche._id}`);
         totalProduits.appendChild(carteLink);
-        document.getElementById(`link-${peluche._id}`).href="./optionProduit.html";
+        document.getElementById(`link-${peuluche._id}`).href="./optionProduit.html";
 
         let peulucheCarte = document.createElement("div");
-        peulucheCarte.setAttribute("id",`carte-${peluche._id}`);
-        peulucheCarte.setAttribute("class","peluche-carte");
+        peulucheCarte.setAttribute("id",`carte-${peuluche._id}`);
+        peulucheCarte.setAttribute("class","peuluche-carte");
         carteLink.appendChild(peulucheCarte);
         peulucheCarte.style.height = "450px";
 
         let peulucheImage = document.createElement("img");
-        peulucheImage.setAttribute("id",`image-${peluche._id}`);
+        peulucheImage.setAttribute("id",`image-${peuluche._id}`);
         peulucheImage.setAttribute("class","peuluche-image");
         peulucheCarte.appendChild(peulucheImage);
         peulucheImage.style.height = "320px";
-        document.getElementById(`image-${peluche._id}`).src = peuluche.imageUrl;
+        document.getElementById(`image-${peuluche._id}`).src = peuluche.imageUrl;
 
         let peulucheTitre = document.createElement("h3");
-        peulucheTitre.setAttribute("id",`titre-${peluche._id}`);
-        peulucheTitre.setAttribute("class","peluche-titre");
+        peulucheTitre.setAttribute("id",`titre-${peuluche._id}`);
+        peulucheTitre.setAttribute("class","peuluche-titre");
         peulucheCarte.appendChild(peulucheTitre);
-        document.getElementById(`titre-${peluche._id}`).innerHTML = peuluche.name;
+        document.getElementById(`titre-${peuluche._id}`).innerHTML = peuluche.name;
 
         let peulucheDesp = document.createElement("p");
-        peulucheDesp.setAttribute("id",`description-${peluche._id}`);
-        peulucheDesp.setAttribute("class","peluche-desp");
+        peulucheDesp.setAttribute("id",`description-${peuluche._id}`);
+        peulucheDesp.setAttribute("class","peuluche-desp");
         peulucheCarte.appendChild(peulucheDesp);
-        document.getElementById(`description-${peluche._id}`).innerHTML = peuluche.description;
+        document.getElementById(`description-${peuluche._id}`).innerHTML = peuluche.description;
         
         let peuluchePrice = document.createElement("p");
         peuluchePrice.setAttribute("id",`price-${peuluche._id}`);
         peuluchePrice.setAttribute("class","peuluche-price")
         peulucheCarte.appendChild(peuluchePrice);
         document.getElementById(`price-${peuluche._id}`).innerHTML =`Price:${(peuluche.price/100).toFixed(2)}€`;
-
-         
     }
+        
     let totalProduits = document.querySelector("#total-produits");
     totalProduits.style.display = "flex";
-                    totalProduits.style.flexWrap = "wrap";
-                    totalProduits.style.justifyItems = 'center';  
-    
-
-}
-
+    totalProduits.style.flexWrap = "wrap";
+    totalProduits.style.justifyItems = 'center';
+ }   
 elementAppend();
+
+
+
