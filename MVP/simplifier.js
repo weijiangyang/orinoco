e53f24fc7eba0d71b322cd83c558d1131a095afa
weijiangyang <weijiangyang@laposte.net>
2021-06-22 11:40:26 +0200

    --- Auto-Git Commit ---

--- MVP/simplifier.js
+++ MVP/simplifier.js
@@ -1,9 +1,22 @@
 for(let i=0; i<5;i++)
-let produitOfPanier = `monPanier[${i}]`
+let produitOfPanier = `${monPanier[i]}`
+let nomProduit = `${produitOfPanier.name}`
+let despProduit = `${produitOfPanier.description}`
+let prixProduit = `${produitOfPanier.price}`
+let quantityProduit = `${produitOfPanier.quantity}`
+let prixProduitText = "Price:"+(prixProduit/100).toFixed(2)+"&nbsp€"+`(*${quantityProduit})`
+
 let idCarte = `carte-${i}`
+let idInfProd =`infor-produit-${i}`
+let idPrixProd = `price-produit-${i}`
 let htmlElement = `<div class="carte" id="idCarte">
 let srcImg = produitOfPanier.imageUrl
 <div class="image-produit">
 <img src="srcImg">
+</div>
+<div class="info-produit" id="idInfProd">
+<h2 class="nom-peluche">nomProduit</h2>
+<p class="desp-produit">despProduit</p>
+<p class="price-produit" id="idPrixProd"></p>
 </div>
 </div>`
