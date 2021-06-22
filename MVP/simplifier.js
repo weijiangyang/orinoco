45bcc5a41f3a33c2346073fd329018cffc330e13
weijiangyang <weijiangyang@laposte.net>
2021-06-22 11:10:07 +0200

    --- Auto-Git Commit ---

---
+++ MVP/simplifier.js
@@ -0,0 +1,9 @@
+for(let i=0; i<5;i++)
+let produitOfPanier = `monPanier[${i}]`
+let idCarte = `carte-${i}`
+let htmlElement = `<div class="carte" id="idCarte">
+let srcImg = produitOfPanier.imageUrl
+<div class="image-produit">
+<img src="srcImg">
+</div>
+</div>`
