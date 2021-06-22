b037b01b4771e0fa59817b913138e3eebf741d4b
weijiangyang <weijiangyang@laposte.net>
2021-06-22 22:17:58 +0200

    --- Auto-Git Commit ---

--- MVP/simplifier.js
+++ MVP/simplifier.js
@@ -1,34 +1,50 @@
 let monPanier = JSON.parse(localStorage.getItem("monPanier"));
 let parentMain = document.querySelector("main");
 let parentPanierInf = document.getElementById("monpanier-inf");
+function appendElement(){
+        for(let i=0; i<monPanier.length;i++){
 
-for(let i=0; i<monPanier.length;i++){
-
-let nomProduit = monPanier[i].name
-let despProduit = monPanier[i].description
-let prixProduit = monPanier[i].price
-let quantityProduit = monPanier[i].quantity
-let colorProduit = monPanier[i].color
+        let nomProduit = monPanier[i].name
+        let despProduit = monPanier[i].description
+        let prixProduit = monPanier[i].price
+        let quantityProduit = monPanier[i].quantity
+        let colorProduit = monPanier[i].color
 
 
-let idCarte = `carte-${i}`
-let idInfProd =`infor-produit-${i}`
-let idPrixProd = `price-produit-${i}`
+        let idCarte = `carte-${i}`
+        let idInfProd =`infor-produit-${i}`
+        let idPrixProd = `price-produit-${i}`
+        let idPrixItemTotal = `pItemTotal-${i}`
+        let srcImg = monPanier[i].imageUrl
 
-let srcImg = monPanier[i].imageUrl
-
-let htmlElement=`<div class="carte" id="${idCarte}">
-                    <div class="image-produit">
-                        <img src="${srcImg}">
-                    </div>
-                    <div class="info-produit" id="${idInfProd}">
-                        <h2 class="nom-peluche">"${nomProduit}"</h2>
-                        <p class="desp-produit">"${despProduit}"</p>
-                        <p class="price-produit" id="${idPrixProd}">Price:${(prixProduit/100).toFixed(2)}&nbsp€(*${quantityProduit})</p>
-                        <p class="color-produit">Color:${colorProduit}</p>
-                        <label class="labelQuantity">Quantity:
-                            <input class="quantity-inp" type="number" value="${quantityProduit}" min="1">
-                        </label>
-                    </div>
-                </div>`
-parentPanierInf.innerHTML+= htmlElement }
+        let htmlElement=`<div class="carte" id="${idCarte}">
+                        <div class="image-produit">
+                            <img src="${srcImg}">
+                        </div>
+                        <div class="info-produit" id="${idInfProd}">
+                            <h2 class="nom-peluche">"${nomProduit}"</h2>
+                            <p class="desp-produit">"${despProduit}"</p>
+                            <p class="price-produit" id="${idPrixProd}">Price:${(prixProduit/100).toFixed(2)}&nbsp€(*${quantityProduit})</p>
+                            <p class="color-produit">Color:${colorProduit}</p>
+                            <label class="labelQuantity">Quantity:
+                                <input class="quantity-inp" type="number" value="${quantityProduit}" min="1">
+                            </label>
+                            <p class="totalprice-produit" id="pItemTotal-${i}">${quantityProduit*(prixProduit/100).toFixed(2)}&nbsp€</p>
+                        </div>
+                    </div>`
+        parentPanierInf.innerHTML+= htmlElement 
+        document.querySelector("input.quantity-inp").addEventListener("change",function(){
+        quantityProduit = this.value;
+        localStorage.setItem("monPanier",JSON.stringify(monPanier));
+        eltP = document.getElementById("${idPrixItemTotal}");
+                    eltP.innerHTML = "Price:"+(monPanier[i].price/100).toFixed(2)+"&nbsp€"+`(*${monPanier[i].quantity}）`;
+                    pItemTotal = document.getElementById(`pItemTotal-${i}`);
+                    pItemTotal.innerHTML = ((monPanier[i].quantity)*(monPanier[i].price/100)).toFixed(2)+"&nbsp€";
+                    sum = 0;
+                    for(let i=0;i<monPanier.length;i++){
+                        sum+=(monPanier[i].quantity)*(monPanier[i].price/100);
+                        }
+                    location.reload();
+                    }
+                ) }}
+            appendElement();
