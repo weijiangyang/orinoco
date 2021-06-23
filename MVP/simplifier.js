4f52a309793560b91e43fdc61dd06cc7e3769f63
weijiangyang <weijiangyang@laposte.net>
2021-06-23 06:59:02 +0200

    --- Auto-Git Commit ---

--- MVP/simplifier.js
+++ MVP/simplifier.js
@@ -1,6 +1,7 @@
 let monPanier = JSON.parse(localStorage.getItem("monPanier"));
 let parentMain = document.querySelector("main");
 let parentPanierInf = document.getElementById("monpanier-inf");
+function append(){
 
         for(let produit of monPanier){
 
@@ -33,19 +34,25 @@ let parentPanierInf = document.getElementById("monpanier-inf");
                         </div>
                     </div>`
         parentPanierInf.innerHTML+= htmlElement 
-        document.querySelector("input").addEventListener("change",function(){
+        const inputQuantity = document.querySelector("input")
+        inputQuantity.addEventListener("change",function(){
         quantityProduit = this.value;
         localStorage.setItem("monPanier",JSON.stringify(monPanier));
+        
         eltP = document.querySelector("p.price-produit")
                     eltP.innerHTML = "Price:"+`${(prixProduit/100).toFixed(2)}`+"&nbsp€"+`(*${quantityProduit}）`;
                     pItemTotal = document.querySelector("p.totalprice-produit");
-                    pItemTotal.innerHTML = `${(quantityProduit*(prixProduit/100)).toFixed(2)}`+"&nbsp€";
-                    // document.getElementById(`${idPrixProd}`).innerHTML = "50";
-                    sum = 0;
-                    for(let i=0;i<monPanier.length;i++){
-                        sum+=(quantityProduit)*(prixProduit/100)
-                        }
+                    pItemTotal.innerHTML = `${(quantityProduit*(prixProduit/100)).toFixed(2)}`+"&nbsp€";})
+        //             document.getElementById(`${idPrixProd}`).innerHTML = "50";
+        //             sum = 0;
+        //             for(let i=0;i<monPanier.length;i++){
+        //                 sum+=(quantityProduit)*(prixProduit/100)
+        //                 }
+                       
                     
-                    }
-                ) 
-            }
+        //             }
+                // ) 
+            // }
+        
+    }}
+            append()
