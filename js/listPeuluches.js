bfaa0ff88c5889fe4bd23319affcce4dcc26ce55
weijiangyang <weijiangyang@laposte.net>
2021-07-20 10:25:16 +0200

    changer addresse de produit

--- js/listPeuluches.js
+++ js/listPeuluches.js
@@ -17,7 +17,7 @@ function getPeulucheInf(){
         function elementAppend(){
             for (let peuluche of peuluches){
                 let htmlElement = 
-                    `<a class="lien" href="./html/optionProduit.html">
+                    `<a class="lien" href="./html/optionProduit.html?${peuluche.name}">
                         <div class="peuluche-carte" style="height:450px">
                             <img class="peuluche-image" src= "${peuluche.imageUrl}" style="height:320px">
                             <h3 class="peuluche-titre">${peuluche.name}</h3>
